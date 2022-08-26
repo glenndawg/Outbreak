5cb80df807f814f5b3fe590c633c2c7d0d00818c
Glenn MacRae <glennie@gmail.com>
2022-08-25 17:23:25 -0700

    load json files and format classes

--- Models/city.py
+++ Models/city.py
@@ -1,14 +1,36 @@
 import random
+import json
 class City:
 
-    def __init__(self, name: str, population: str, infected_population = 0, r0 = 2):
+    def __init__(self, name: str, population: str, infected_population = 0.0, r0 = 0.0, unemployment_rate = 0.0, open_hires = 0.0 ):
         self.name = name
         self.population = population
         self.infected_population = infected_population
-        self.r0 = r0
+        self.r0 = round(r0,2)
+        self.unemployment_rate = round(unemployment_rate,2)
+        self.open_hires = open_hires
         
-    def start_emp(self, unemp_rate = 0):
-        self.unemp_rate = random.randint(7,12)
+    def start_unemployment_rate(self, unemployment_rate: float):
+        self.unemployment_rate = random.randint(7,12)
 
     def openhires(self, open_hires = 0):
         self.open_hires = int(round((self.unemp_rate / 100) * self.population))
+
+    def __repr__(self):
+        return 'Name: {} \n Population: {} \n Infected Population: {} \n r0: {} \
+                  \n Unemployment rate: {} \n Open hires: {}'.format( self.name,
+                                                                self.population,
+                                                                self.infected_population,
+                                                                round(self.r0,2),
+                                                                round(self.unemployment_rate,2),
+                                                                self.open_hires)
+
+def load_cities(file_path: str, usa_cities: list[City]):
+    with open(file_path, 'r') as f:
+        loaded_cities_list = json.load(f)
+        for city in loaded_cities_list:
+            usa_cities.append(City( name=city['name'],
+                                    population=city['population'],
+                                    r0=city['r0']))
+
+        
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from Models import city , event_cards
+from pprint import pprint
+from Models.city import load_cities, City
+from Models.event_cards import load_events, EventCard
 from singletons import virus
-
 import random
-from decimal import ROUND_05UP
-from unicodedata import name
+
 
 # Your code challenge is to load in all of the data for event cards and cities, 
 # and use a loop inside of main.py to simulate 25 weeks with a COVID outbreak. 
@@ -13,17 +13,40 @@ from unicodedata import name
 # You will have 1 virus object instantiated in the main method that you will use 
 # throughout the simulation.
 
-week_count = 25
-city_list = []
+CITIES_JSON_FIXTURES_FILE_PATH = './fixtures/cities.json'
+EVENT_CARDS_JSON_FIXTURES_FILE_PATH = './fixtures/event_cards.json'
 
+week_count = 5
+USA_CITIES = []
+CARD_DECK = []
+
+
+# r0 and unemp_rate are public instead of private
 # country class? list?
 # if is federal, then apply federally, then use defined function
 # def apply_federally(event.card, lity_list): Change all city.r0 and city.unemp_rate
 
+def apply_federally(event_card, city_list_for_country):
+        for city in city_list_for_country:
+            city.r0 += event_card.r0_difference
+            city.start_unemployment_rate
+            # city.unemployment_rate += event_card.unemployment_rate_difference
+        return city_list_for_country
+
 def main():
-    for week in range(len(week_count)):
 
+    load_cities(CITIES_JSON_FIXTURES_FILE_PATH,USA_CITIES)
+    load_events(EVENT_CARDS_JSON_FIXTURES_FILE_PATH,CARD_DECK)
 
-
+    for week in range(week_count):
+        random_event = random.choice(CARD_DECK)
+        print('Random event is {}'.format(random_event.name))
+        if random_event.area_affected == 'federal':
+            pprint(apply_federally(random_event, USA_CITIES))
+        elif random_event.area_affected == 'local':
+            random_city = random.choice(USA_CITIES)
+            pprint(random_event.apply_to_affected_citie(random_city))
+            # if the event is local, choose a city at random
+        
 if __name__ == '__main__':
     main()
